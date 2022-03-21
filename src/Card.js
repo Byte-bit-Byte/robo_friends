40b573ed4943aaf29aa78b44a1bcc5afddbf7be9
Efe Erhie <efe_erhie@git_commits.me>
2022-03-21 11:32:44 -0500

    Basic responsive app build complete

--- src/Card.js
+++ src/Card.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
 
-class Hello extends Component{
-  constructor(props) {
-    super(props);
-  }
+
+class Card extends Component{
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render(){
     return (
-    <div className="f1 tc">
-      <header className="Hello-header">
-      </header>
-      <h1>Hello World</h1>
-      <p> Welcome to React</p>
+    <div className='tc bg-light-green br3 ma2 pa3 grow bw2 shadow-5 w-100 w-80-m w-25-l'>
+      {/*<h1 className='tc'> Robo Friends</h1>*/}
+      <img src={`https://robohash.org/set_setany/${this.props.id}?size=200x200`} alt={`robo version of ${this.props.name}`} />
+      <div>
+        <h2>{this.props.name}</h2>
+        <p>{this.props.email}</p>
+      </div>
     </div>
   );
   }  
 }
 
-export default Hello;
+export default Card;
