71f45deb8e14842d2cc563a28164053c81d427f0
Efe Erhie <efe_erhie@git_commits.me>
2022-04-01 15:21:58 -0500

    Added ErrorBoundary.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 class App extends Component{
   constructor(props){
@@ -47,7 +48,9 @@ class App extends Component{
           <h1 className='f1'>Robo Friends</h1>
           <SearchBox searchChange={this.handleChange}/>
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
